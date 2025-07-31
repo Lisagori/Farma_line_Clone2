54133f00e4825b6d53901973f8b5624c597e829c
ila-i <grasso.ilaria4@gmail.com>
2025-07-31 10:13:17 +0200

    creazione funzione accesso

--- homepage/main.py
+++ homepage/main.py
@@ -1,4 +1,7 @@
 from profile_creation.iscrizione import registrazione_utente
+from accesso.accesso import  accesso_utente
+
+print("HOME PAGE")
 
 controll ="go"
 print( "Se si vuole terminare scrivere exit quando richiesto ")
