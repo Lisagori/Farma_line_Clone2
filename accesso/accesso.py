f0f435282f02c4a6c46d7ca6b874999d907c3d85
ila-i <grasso.ilaria4@gmail.com>
2025-07-31 13:02:08 +0200

    aggiunta opzione iscrizione da accesso

--- accesso/accesso.py
+++ accesso/accesso.py
@@ -4,21 +4,33 @@ from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def accesso_utente() -> bool :
+def accesso_utente() -> str:
     username :str
     pw : str #pw abbrevviazione per password
+    count : int
+    count = 3
 
     #il termice chek viene utilizzato per riferire le variabili di controllo usate per verificare la presenza dell'utente
+
+    #sezione dedicata al controllo del nome utente
     username = input("Inserire il proprio nome utente : ")
     username_check = session.query(ProfiloUtenteDB).filter_by(nome_utente=username).first()
     while not username_check  :
-        username = input(" Il nome utente inserito non appartiente a un utente registarto, riprovare : ")
+        username = input(f" Il nome utente inserito non appartiente a un utente registarto, riprovare (tentativi rimasti {count}): ")
         username_check  = session.query(ProfiloUtenteDB).filter_by(nome_utente=username).first()
+        count -= 1
+        if count == 0 :
+            print("se non si è in possesso di un profilo utente già registrato selezionare 2 per iscriversi al servizio")
+            print("digitare exit se si vuole terminare le operazioni")
+            verify = input()
+            return verify#si riconduce al main dove fa la nuova iscrizione
 
-    pw = input ("Inserire la propria password : ")
-    pw_check = session.query(ProfiloUtenteDB).filter_by(nome_utente=username, password = pw).first()
-    while not pw_check :
-        pw = input(" La password inserita  per questo username è incorretta, riprovare : ")
-        pw_check = session.query(ProfiloUtenteDB).filter_by(nome_utente=username, password = pw).first()
+    if count > 0 :
+            #sezione dedicata al controllo password
+            pw = input ("Inserire la propria password : ")
+            pw_check = session.query(ProfiloUtenteDB).filter_by(nome_utente=username, password = pw).first()
+            while not pw_check :
+                pw = input(" La password inserita  per questo username è incorretta, riprovare : ")
+                pw_check = session.query(ProfiloUtenteDB).filter_by(nome_utente=username, password = pw).first()
 
 
--- homepage/main.py
+++ homepage/main.py
@@ -1,17 +1,20 @@
 from profile_creation.iscrizione import registrazione_utente
 from accesso.accesso import  accesso_utente
 
+scelta : str
 print("HOME PAGE")
-scelta= int(input("""Se si è in possesso di un profilo utente digitare 1 per accedere al servizio.
+scelta= input("""Se si è in possesso di un profilo utente digitare 1 per accedere al servizio.
 Se non si possiede un profilo utente digitare 2 per registrarsi al servizio. 
-"""))
+""")
 
-if scelta == 1 :
-    accesso_utente()
+while scelta == "1" :
+    scelta = accesso_utente()
 
-elif scelta ==2 :
+if scelta == "2" :
     if registrazione_utente() :
         print("seguire le istruzioni per accedere al servizio")
         accesso_utente()
     else :
         print(" Operazione terminata ")
+else :
+    print(" Operazione terminata ")
