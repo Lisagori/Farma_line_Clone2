744551d82c9a72d4cb92c290e87f252ff3ed6bef
Lisagori <Lisa.gori@edu.unifi.it>
2025-08-11 23:00:58 +0200

    risoluzione problema scelta indirizzi

--- acquisto/scelta_indirizzi.py
+++ acquisto/scelta_indirizzi.py
@@ -34,16 +34,6 @@ def acquisto_farmaci() ->None :
             if nome_ck.empty :
                 print("Non è associata nessuna ricetta per questo farmaco al profilo corrente, il prodotto verrà eliminato dal carrello")
                 carrello.remove(prodotto)
-        # 1) Vedi cosa c'è in Ricette per quel farmaco
-        dbg = pd.read_sql_query(
-            sa.text("SELECT codice_farmaco, '['||codice_fiscale||']' as cf FROM Ricette WHERE codice_farmaco = :cod"),
-            connection, params={"cod": codice_val}
-        )
-        print("Ricette per farmaco:", codice_val)
-        print(dbg.to_string(index=False))
-
-        # 2) Stampa il CF che stai usando
-        print("CF usato:", f"[{codice_fiscale_utente}]")
 
         if serve_ricetta.empty:
             print("per ricevere l'ordine a domicilio digitare 1")
@@ -57,5 +47,6 @@ def acquisto_farmaci() ->None :
         elif scelta == "2":
             print("seleziona una farmacia per il ritiro dei prodotti tra gli indirizzi disponibili")
             print("servizio non disponibile")  # TODO aggiungi tabella indirizzi
+
 
 
